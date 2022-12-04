a12b2a5c3a49763f8e0d50a27b4f67b5121ca096
AlinaRinn <nik17042001@rambler.ru>
2022-12-04 19:03:37 +0300

    Small fixes

--- GoldenRatio/GoldenRatio.cpp
+++ GoldenRatio/GoldenRatio.cpp
@@ -3,21 +3,24 @@
 // 22 2x^2-12x min [0; 2] 0.01
 
 #include <iostream>
+#include <chrono>
 
 const double GOLDEN_NUMBER = 1.6180339887498948482045868343656381177203091798057628621354486227052604628189024497072072041893911374;
-const double E = 0.00000000000001, // Error
-             A = 0,    // Upper bound
-             B = 2;    // Lower bound
+const double E = 0.01, // Error
+             A = 0,       // Upper bound
+             B = 2;     // Lower bound
 
 double function(double x) {
     return 2 * x * x - 12 * x;
+    //return exp(-2*pow(x,15))+x*x/2-log(45);
 }
 
 double search(double A, double B, double GLDN_NMBR, int &iter) {
     double x1 = 0, x2 = 0;
     double fx1 = 0, fx2 = 0;
 
-    while (abs(B-A) > E) {
+    // I dont make iteration constraint because i cant cause inf loop at any meaningful error value. Also dont want make program slower
+    while (abs(B-A) > E) { 
         x1 = B - ((B - A) / GLDN_NMBR);
         x2 = A + ((B - A) / GLDN_NMBR);
         fx1 = function(x1);
@@ -36,12 +39,23 @@ double search(double A, double B, double GLDN_NMBR, int &iter) {
 
 int main()
 {
+    auto begin = std::chrono::high_resolution_clock::now();
     int iter = 0;
-    double minimum = search(A, B, GOLDEN_NUMBER, iter);
+    double minimum = search(A, B, GOLDEN_NUMBER, iter),
+           func = function(minimum);
+    auto end = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Minmum of stated function is: " << minimum
-              << "\nFunction value in this point is: " << function(minimum)
-              << "\nIteration count: " << iter << "\n\n\n";
+    std::cout << "Minmum of given function is:      " << minimum
+              << "\nFunction value in this point is: " << func;
+   
+    std::chrono::duration<float> duration = end - begin;
+
+    std::cout << "\nRuntime: " << (float)duration.count() << " sec"
+              << "\nIteration count: " << iter << "\nError: " << E << "\n\n\n";
 
     system("pause");
 }
+
+
+
+
